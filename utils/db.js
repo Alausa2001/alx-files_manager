238d308ddb5ccb0e3dd80947eb21342975c11fa1
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-04-02 23:46:17 +0100

    mongodb client connection

---
+++ utils/db.js
@@ -0,0 +1,34 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const dbHost = process.env.DB_HOST || 'localhost';
+    const dbPort = process.env.DB_PORT || 27017;
+    const db = process.env.DB_DATABASE || 'files_manager';
+
+    this.mongoClient = new MongoClient(`mongodb://${dbHost}:${dbPort}/${db}`);
+    this.mongoClient.connect();
+  }
+
+  isAlive() {
+    if (this.mongoClient.isConnected) return true;
+    return false;
+  }
+
+  async nbUsers() {
+    this.database = this.mongoClient.db();
+    this.users = this.database.collection('users');
+    const countUsers = await this.users.countDocuments();
+    return countUsers;
+  }
+
+  async nbFiles() {
+    this.database = this.mongoClient.db();
+    this.files = this.database.collection('files');
+    const countFiles = this.files.countDocuments();
+    return countFiles;
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
